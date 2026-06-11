d335932a295ecd6ce08747cef533296498b24f5f
amrw28940-byte <amrw28940@gmail.com>
2026-06-11 06:26:31 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -148,17 +148,17 @@ export default function Portfolio() {
   };
 
   const itemVariants: any = {
-  hidden: { opacity: 0, y: -40 },
-  show: { 
-    opacity: 1, 
-    y: 0, 
-    transition: { 
-      type: "spring", 
-      stiffness: 100, 
-      damping: 15 
-    } 
-  }
-};
+    hidden: { opacity: 0, y: -40 },
+    show: { 
+      opacity: 1, 
+      y: 0, 
+      transition: { 
+        type: "spring", 
+        stiffness: 100, 
+        damping: 15 
+      } 
+    }
+  };
 
   return (
     <>
@@ -197,8 +197,9 @@ export default function Portfolio() {
 
       <div className="min-h-screen bg-slate-950 text-white relative overflow-x-hidden font-sans pb-12" dir={t.dir}>
         
-        <div className="fixed inset-0 opacity-100 pointer-events-none grid grid-cols-6 sm:grid-cols-10 md:grid-cols-16 gap-3 p-3 z-10 overflow-hidden w-full h-full drop-shadow-[0_0_35px_rgba(52,211,153,0.6)]">
-          {Array.from({ length: 320 }).map((_, i) => (
+        {/* تم تعديل الـ opacity إلى 0.05 لتقليل السطوع */}
+        <div className="fixed inset-0 opacity-0.05 pointer-events-none grid grid-cols-6 sm:grid-cols-10 md:grid-cols-16 gap-3 p-3 z-10 overflow-hidden w-full h-full drop-shadow-[0_0_35px_rgba(52,211,153,0.6)]">
+          {Array.from({ length: 200 }).map((_, i) => (
             <motion.div
               key={i}
               className="h-6 w-full rounded-sm shadow-[0_0_25px_#10b981,0_0_45px_#34d399]"
